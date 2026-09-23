faf97f2d9e403941c70f991b6e3051b9dfe920e2
Keli Deng <673188101@qq.com>
2026-09-23 16:50:03 +0800

    Add OneDrive SPA configuration placeholder

---
+++ assets/js/onedrive-config.js
@@ -0,0 +1,5 @@
+// Public SPA configuration. Microsoft Application (client) ID is not a secret.
+// After registering SimpleSchedule in Microsoft Entra, paste the client ID below.
+window.SIMPLE_SCHEDULE_CONFIG = Object.freeze({
+  microsoftClientId: ''
+});
